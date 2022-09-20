21f152f5abd9296ffe0bb6943af26d7af14b644b
らいか <raika4789@gmail.com>
2022-09-20 17:55:15 +0900

    argc != 2の時は弾くように、pathのチェック方式の変更

--- error_check.c
+++ error_check.c
@@ -6,7 +6,7 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 10:46:11 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/09/19 20:22:16 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/09/20 17:54:03 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 int	path_check(char *path)
 {
-	int	i;
+	size_t	i;
 
-	i = 0;
-	if (path == NULL)
-		return (-1);
+	i = ft_strlen(path);
 	if (path[0] == '.')
 		return (-1);
-	while (path[i] != '\0')
+	while (i > 0)
 	{
-		if (i == INT_MAX)
+		if (i > INT_MAX)
 			return (-1);
 		if (path[i] == '.')
 		{
-			if (ft_strncmp(&path[i], ".bar", 5) != 0)
+			if (ft_strncmp(&path[i], ".ber", 5) != 0)
 				return (-1);
+			else 
+				return (0);
 		}
-		i ++;
+		i --;
 	}
 	return (0);
 }
